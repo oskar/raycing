a642176363504750e28ea0646d3654dc45fb16ad
korla <johannes.bengtsson@raysearchlabs.com>
2015-12-26 01:26:00 +0100

    Vue router

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
 var Vue = require('vue');
+var VueRouter = require('vue-router');
+Vue.use(VueRouter);
 
 var mainMenu = require('./views/mainMenu.vue');
 
 var attachFastClick = require('fastclick');
 
-new Vue({
-  el: 'body',
-  components: {
-    mainMenu
-  }
-});
-
 window.addEventListener('load', () => {
   attachFastClick.attach(document.body);
 }, false);
+
+var App = Vue.extend({});
+
+var router = new VueRouter();
+
+router.map({
+    '/': {
+        component: mainMenu
+    },
+});
+
+router.start(App, 'body');
