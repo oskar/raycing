b15ce244a03d5e227c1e8d7c110399bc98281080
korla <johannes.bengtsson@raysearchlabs.com>
2016-01-10 23:29:19 +0100

    Remove alert when winning game

--- src/vues/play.vue
+++ src/vues/play.vue
@@ -1,5 +1,7 @@
 <template>
-    <svg-menu :menu="'none'" :small-buttons="true"></svg-menu>
+    <svg-menu :menu="model.menu" :small-buttons="model.smallButtons">
+      <div class="text-medium playMenuBottom" slot="menuBottom">{{model.text}}</div>
+    </svg-menu>
 </template>
 
 <script lang="babel">
@@ -11,6 +13,11 @@
   import Game from '../game/game';
   import Player from './player';
 
+  var model = {
+    text: '',
+    menu: 'none',
+    smallButtons: true
+  };
   var course;
   var game;
   var controls;
@@ -23,6 +30,9 @@
   var gestureendListener;
 
   function created() {
+    model.text = '';
+    model.menu = 'none';
+    model.smallButtons = true;
     var numberOfPlayers = parseInt(this.$route.params.playerCount);
     var jsonMap = storage.Get(this.$route.params.key).map;
     var track = Paper.project.importJSON(jsonMap.Track);
@@ -127,18 +137,11 @@
 
   function endGame(endState) {
     clearControls();
-
-    var text = '';
-    if(endState.winningPlayerIndex < 0) {
-      text = 'Everybody crashed, you all lost!';
-    }
-    else {
-      var winner = 'Player ' + (endState.winningPlayerIndex + 1);
-      text = 'Game over, ' + winner + ' won in ' + endState.moves + ' moves!';
-    }
-
-    // yeah..
-    alert(text);
+    model.menu = '';
+    model.smallButtons = false;
+    model.text = endState.winningPlayerIndex < 0 ?
+      'Everybody crashed, you all lost!' :
+      `Game over, player ${endState.winningPlayerIndex + 1} won in ${endState.moves} moves!`;
   }
 
   function onMouseDown(event) {
@@ -165,9 +168,7 @@
     created,
     destroyed,
     data() {
-      return {
-
-      }
+      return { model }
     },
     components: {
       svgMenu: require('./svgMenu.vue')
@@ -177,3 +178,11 @@
     }
   }
 </script>
+
+<style>
+  .playMenuBottom {
+    width: 40%;
+    left: 30%;
+    padding-top: 7vh;
+  }
+</style>
